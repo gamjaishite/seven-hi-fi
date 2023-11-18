26f033d0237f71a5dc2190f2b2ce862ebff6e67c
irsyadnb <irsyadxd@gmail.com>
2023-11-18 21:25:59 +0700

    secondary navbar template

--- src/components/PageTemplate.js
+++ src/components/PageTemplate.js
@@ -1,10 +1,11 @@
 import Breadcrumb from "@/components/Breadcrumb";
-import Footer from "@/components/Footer";
+import SecondaryNavbar from "@/components/SecondNavBar";
 
-const PageTemplate = ({ children, pageTitle, breadCrumbs }) => {
+const PageTemplate = ({ children, pageTitle, breadCrumbs, secondarynavbars }) => {
   return (
     <div className="flex flex-1 flex-col px-4 gap-4 xl:px-0 py-5 max-w-[1140px] mx-auto w-full">
       <Breadcrumb breadCrumbs={breadCrumbs} />
+      {secondarynavbars ? <SecondaryNavbar secNav={secondarynavbars}/> : null}
       <h2 className="text-seven-font-size-page-title text-seven-page-heading font-light">
         {pageTitle}
       </h2>
