70df9855d613afce19388621d957722f1c34a1ee
Enliven26 <16521443@mahasiswa.itb.ac.id>
2023-11-18 20:11:54 +0700

    Update PageTemplate.js

--- src/components/PageTemplate.js
+++ src/components/PageTemplate.js
@@ -5,7 +5,7 @@ const PageTemplate = ({children, pageTitle, breadCrumbs}) => {
     return ( 
         <div className="flex flex-1 flex-col px-4 gap-4 xl:px-0 py-5 max-w-[1140px] mx-auto w-full">
             <Breadcrumb  breadCrumbs={breadCrumbs}/>
-            <h2 className="text-4xl text-seven-page-heading font-light py-1">
+            <h2 className="text-seven-page-heading font-light py-1">
             {pageTitle}
             </h2>
             {children}
