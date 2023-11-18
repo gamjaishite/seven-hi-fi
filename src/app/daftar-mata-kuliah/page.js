ce43899235722070c95d0b4981162935a5aed317
Enliven26 <16521443@mahasiswa.itb.ac.id>
2023-11-19 00:59:14 +0700

    feat: filter mata kuliah

--- src/app/daftar-mata-kuliah/page.js
+++ src/app/daftar-mata-kuliah/page.js
@@ -1,8 +1,22 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select";
 import Link from "next/link";
+import { useState } from "react";
+import { FaFilter, FaSearch } from "react-icons/fa";
 
 const { default: PageTemplate } = require("@/components/PageTemplate")
 
-const subjects = [
+const DEFAULT_SUBJECTS = [
     {code: 'KKXXXX', name: 'Nama Mata Kuliah', faculty: 'FXXX', study: 'Nama Program Studi', category: 'Wajib', schedule_href: '/'},
     {code: 'KKXXXX', name: 'Nama Mata Kuliah', faculty: 'FXXX', study: 'Nama Program Studi', category: 'Wajib', schedule_href: '/'},
     {code: 'KKXXXX', name: 'Nama Mata Kuliah', faculty: 'FXXX', study: 'Nama Program Studi', category: 'Wajib', schedule_href: '/'},
@@ -46,7 +60,7 @@ const SubjectRow = ({subject, idx}) => {
     )
 }
 
-const SubjectsTable = () => {
+const SubjectsTable = ({subjects = []}) => {
     return (
         <div className="grid border border-seven-border-grey rounded-sm w-fit md:w-full">
             <table className="text-left text-seven-foreground-light w-[710px] md:w-full">
@@ -77,10 +91,54 @@ const SubjectsTable = () => {
         </div>
     )
 }
+
+const SubjectFilter = ({label, onChange}) => {
+    return (
+        <Select onValueChange={onChange}>
+            <SelectTrigger className="text-[12px] space-x-2 border border-seven-border-filter min-w-max py-[5px] px-[10px] h-fit text-seven-filter">
+                <SelectValue className="" placeholder={label} />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectGroup>
+                <SelectLabel>2023/2024</SelectLabel>
+                <SelectItem value="1-2023/2024">Semester 1 - 2023/2024</SelectItem>
+                </SelectGroup>
+                <SelectGroup>
+                <SelectLabel>2022/2023</SelectLabel>
+                <SelectItem value="3-2022/2023">Semester 3 - 2022/2023</SelectItem>
+                <SelectItem value="2-2022/2023">Semester 2 - 2022/2023</SelectItem>
+                </SelectGroup>
+            </SelectContent>
+        </Select>
+    )
+}
 const DaftarMataKuliah = () => {
+
+    const [subjects, setSubjects] = useState(DEFAULT_SUBJECTS);
+    const [showFilter, setShowFilter] = useState(false);
 
     return ( 
         <PageTemplate pageTitle='Daftar Mata Kuliah' breadCrumbs={[{href: '/daftar-mata-kuliah', label: 'Daftar Mata Kuliah'}]}>
+            <div className="flex flex-col gap-5 sm:flex-row sm:justify-between">
+                <div className="flex flex-row w-full">
+                    <input maxLength={255} className="border border-seven-border-button-primary rounded-l-sm outline-none text-seven-filter text-seven-font-size-filter px-[12px] w-full"></input>
+                    <Button className="h-fit px-[12px] py-[8px] bg-seven-bg-button-primary border border-l-0 border-seven-border-button-primary rounded-none rounded-r-sm hover:bg-seven-bg-button-primary-hover">
+                        <FaSearch size={12}/>
+                    </Button>
+                </div>
+                <div className="flex flex-row gap-4 items-center w-full sm:w-auto">
+                    {showFilter &&
+                        <div className="flex flex-row gap-4 items-center w-full">
+                            <SubjectFilter label='Fakultas'/>
+                            <SubjectFilter label='Prodi'/>
+                            <SubjectFilter label='Wajib'/>
+                        </div>
+                    }
+                    <Button className="h-fit px-[12px] py-[8px] bg-seven-bg-button-primary border border-seven-border-button-primary hover:bg-seven-bg-button-primary-hover" onClick={() => {setShowFilter(oldValue => !oldValue)}}>
+                        <FaFilter size={12}/>
+                    </Button>
+                </div>
+            </div>
             <SubjectsTable subjects={subjects}/>
         </PageTemplate>
     );
