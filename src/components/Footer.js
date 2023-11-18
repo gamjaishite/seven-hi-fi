ca5ca596d9d238335a647dc5fd31179361496a9e
Enliven26 <16521443@mahasiswa.itb.ac.id>
2023-11-18 20:19:38 +0700

    fix: font size consistency

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,11 +11,11 @@ export default function Footer() {
     <>
       <div className="w-full h-[110px] flex px-4 bg-seven-bg-bread justify-center items-center">
         <div className="max-w-[1140px] w-full flex-col">
-          <span className="text-4xl text-seven-page-heading font-light py-1">
+          <span className="text-seven-font-size-footer-title text-seven-page-heading font-light py-1">
             Institut Teknologi Bandung
           </span>
-          <div className="flex items-center gap-1">
-            Direktorat Pendidikan
+          <div className="flex items-center gap-1 text-seven-font-size-footer-icons">
+            <span className="text-seven-font-size-footer-body">Direktorat Pendidikan</span>
             <FaInfoCircle className="text-seven-hyperlink" />
             <FaInstagram className="text-seven-hyperlink" />
             <FaTwitter className="text-seven-hyperlink" />
