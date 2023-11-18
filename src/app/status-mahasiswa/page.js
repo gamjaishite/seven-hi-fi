0713e23f1167045536c05ea01b1136d02328f660
sozyGithub <thinpotatogithub@gmail.com>
2023-11-18 23:12:01 +0700

    add: secondary navbar on kalender page

--- src/app/status-mahasiswa/page.js
+++ src/app/status-mahasiswa/page.js
@@ -1,11 +1,12 @@
-
 import PageTemplate from "@/components/PageTemplate";
 
 export default function StatusMahasiswa() {
   return (
     <>
-      <PageTemplate pageTitle='Status Mahasiswa' breadCrumbs={[{href: '/', label: 'Jadwal Perkuliahan Mahasiswa'}]}>
-      </PageTemplate>
+      <PageTemplate
+        pageTitle="Status Mahasiswa"
+        breadCrumbs={[{ href: "/", label: "Jadwal Perkuliahan Mahasiswa" }]}
+      ></PageTemplate>
     </>
   );
 }
