b735dfdaafdc66eceb82b03767f77678065bc5cd
sozyGithub <thinpotatogithub@gmail.com>
2023-11-18 22:46:54 +0700

    add: jadwal template, conf, dummy data

--- src/app/jadwal/JadwalConf.js
+++ src/app/jadwal/JadwalConf.js
@@ -1,7 +1,51 @@
 import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { FaCaretLeft, FaCaretRight } from "react-icons/fa";
 
-export default function JadwalConf() {
+function SelectSemester({ semester, setSemester }) {
+  return (
+    <Select defaultValue={semester} onValueChange={setSemester}>
+      <SelectTrigger className="space-x-2 border border-seven-border-filter min-w-max">
+        <SelectValue placeholder="Semester" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>2023/2024</SelectLabel>
+          <SelectItem value="1-2023/2024">Semester 1 - 2023/2024</SelectItem>
+        </SelectGroup>
+        <SelectGroup>
+          <SelectLabel>2022/2023</SelectLabel>
+          <SelectItem value="3-2022/2023">Semester 3 - 2022/2023</SelectItem>
+          <SelectItem value="2-2022/2023">Semester 2 - 2022/2023</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+}
+
+function SelectMode({ mode, setMode }) {
+  return (
+    <Select defaultValue={mode} onValueChange={setMode}>
+      <SelectTrigger className="space-x-2 border border-seven-border-filter">
+        <SelectValue placeholder="Mode" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="month">Bulan</SelectItem>
+        <SelectItem value="week">Minggu</SelectItem>
+      </SelectContent>
+    </Select>
+  );
+}
+
+export default function JadwalConf({ semester, setSemester, mode, setMode }) {
   return (
     <>
       <div className="w-full flex items-center justify-between">
@@ -14,7 +58,10 @@ export default function JadwalConf() {
             <FaCaretRight />
           </Button>
         </div>
-        <div>Right</div>
+        <div className="flex items-center gap-2">
+          <SelectSemester semester={semester} setSemester={setSemester} />
+          <SelectMode mode={mode} setMode={setMode} />
+        </div>
       </div>
     </>
   );
--- src/app/jadwal/page.js
+++ src/app/jadwal/page.js
@@ -1,8 +1,16 @@
-import Breadcrumb from "@/components/Breadcrumb";
+"use client";
+
 import PageTemplate from "@/components/PageTemplate";
 import JadwalConf from "./JadwalConf";
+import { useState } from "react";
+import MonthlyCalendar from "./MonthlyCalendar";
+import WeeklyCalendar from "./WeeklyCalendar";
 
+// Status Enum: "NONE", "ACTIVE", "ATTEND", "NOT ATTEND"
 export default function Jadwal() {
+  const [semester, setSemester] = useState("1-2023/2024");
+  const [mode, setMode] = useState("month");
+
   const breadCrumbItems = [
     { href: "/", label: "Kelas" },
     { href: null, label: "Jadwal Perkuliahan Mahasiswa" },
@@ -12,7 +20,14 @@ export default function Jadwal() {
       breadCrumbs={breadCrumbItems}
       pageTitle="Jadwal Perkuliahan Mahasiswa"
     >
-      <JadwalConf />
+      <JadwalConf
+        semester={semester}
+        setSemester={setSemester}
+        mode={mode}
+        setMode={setMode}
+      />
+      {mode === "month" && <MonthlyCalendar />}
+      {mode === "week" && <WeeklyCalendar />}
     </PageTemplate>
   );
 }
