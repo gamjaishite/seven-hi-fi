5db9c030b38f31dd8c454dda879574cc647610c2
Altair1618 <farhannabilsuryono3@gmail.com>
2023-11-19 21:03:52 +0700

    feat: add color by status in weekly

--- src/components/WeeklyCalendarCard.js
+++ src/components/WeeklyCalendarCard.js
@@ -2,17 +2,36 @@ export function WeeklyCalendarCard(props) {
   const item = props.item;
   const idx = props.idx;
 
-  console.log(item);
+  let bgColor, textColor;
+  switch (item.status) {
+    case "empty":
+      bgColor = "bg-seven-bg-table";
+      textColor = "text-seven-hyperlink";
+      break;
+    case "open":
+      console.log(item.status);
+      bgColor = "bg-seven-bg-presensi-exist";
+      textColor = "text-seven-foreground-dark";
+      break;
+    case "filled":
+      bgColor = "bg-seven-bg-presensi-done";
+      textColor = "text-seven-foreground-dark";
+      break;
+    case "skipped":
+      bgColor = "bg-seven-bg-presensi-skip";
+      textColor = "text-seven-foreground-dark";
+      break;
+  }
 
   return (
     <div
       key={idx}
-      className={`flex flex-col border-seven-border-gray absolute top-[5px] z-10 flex w-[calc(100%-10px)] rounded-lg border bg-[#F8F8F8] px-2.5 py-[5px] text-[12px] font-bold h-[${
+      className={`flex flex-col border-seven-border-gray absolute top-[5px] z-10 flex w-[calc(100%-10px)] rounded-lg border px-2.5 py-[5px] ${bgColor} text-[12px] font-bold h-[${
         50 + 60 * (item.duration - 1)
       }px]`}
     >
-      <p className="text-ellipsis overflow-hidden whitespace-nowrap">{item.start} - {item.end}</p>
-      <p className="text-ellipsis overflow-hidden whitespace-nowrap">{item.matkulCode} <span className="font-normal">{item.matkulName}</span></p>
+      <p className={`text-ellipsis overflow-hidden whitespace-nowrap ${textColor}`}>{item.start} - {item.end}</p>
+      <p className={`text-ellipsis overflow-hidden whitespace-nowrap ${textColor}`}>{item.matkulCode} <span className="font-normal">{item.matkulName}</span></p>
     </div>
   );
 }
