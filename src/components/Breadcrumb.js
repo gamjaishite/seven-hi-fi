f13dfe2965341f41ab23c4a50635c8a924f7f831
Enliven26 <16521443@mahasiswa.itb.ac.id>
2023-11-20 14:12:20 +0700

    Update Breadcrumb.js

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -10,7 +10,7 @@ export default function Breadcrumb({ breadCrumbs }) {
   return (
     <>
       <div className="w-full px-4 py-2 rounded-md flex gap-2 bg-seven-bg-bread items-center text-seven-font-size-default">
-        <a href="/" className="hover:underline text-seven-hyperlink hover:text-seven-hyperlink-hover">SIX</a>
+        <a href="/" className="hover:underline text-seven-hyperlink hover:text-seven-hyperlink-hover">SEVEN</a>
         {breadCrumbs.map((elmt, index) => (
           <React.Fragment key={index}>
             <ChevronsRight className="text-[#ccc]" size={10} />
